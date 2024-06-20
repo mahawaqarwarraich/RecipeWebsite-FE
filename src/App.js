1c07545bd6c060c3aee4e7b3d010b4632893a2c8
UrbaNaeem <148794104+UrbaNaeem@users.noreply.github.com>
2024-06-21 01:09:01 +0500

    error resolved of login and signup

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import './App.css';
 import Home from './components/Home';
-import Recipe from './components/Recipe'; // Assuming the Recipe component is in the components folder
+import Recipe from './components/Recipe';
 import { Routes, Route, useLocation } from "react-router-dom";
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import RecipeDetail from './components/RecipeDetail';
-import RecipeForm from './components/RecipeForm'; // Import the RecipeForm component
-import Login from './components/Login'; // Import the Login component
-import SignUp from './components/SignUp'; // Import the SignUp component
+import RecipeForm from './components/RecipeForm';
+import Login from './components/Login';
+import SignUp from './components/SignUp';
 
 function App() {
   const location = useLocation();
@@ -19,9 +19,9 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/recipes" element={<Recipe />} />
         <Route path="/recipe-detail/:id" element={<RecipeDetail />} />
-        <Route path="/add-recipe" element={<RecipeForm />} /> {/* Add this line to include the RecipeForm route */}
-        <Route path="/login" element={<Login />} /> {/* Add this line to include the Login route */}
-        <Route path="/signup" element={<SignUp />} /> {/* Add this line to include the SignUp route */}
+        <Route path="/add-recipe" element={<RecipeForm />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
       </Routes>
 
       {location.pathname !== '/' && <Footer />}
