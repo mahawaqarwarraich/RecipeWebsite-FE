af310b3b3906fb9e71247c4989b2a9f7fa51a209
Maha <maha.waqar.warraich@gmail.com>
2024-06-21 12:13:50 +0500

    added heart icon

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -16,9 +16,21 @@ const Navbar = () => {
                         <NavLink className='cursor-pointer hover:underline' to='/add-recipe'>Upload Recipe</NavLink>
                     </div>
 
-                    <div className='hidden sm:flex space-x-3 font-semibold'>
-                        <NavLink className='text-white' to='/login'>Login</NavLink> {/* Use NavLink instead of button */}
-                        <NavLink className='bg-white text-green-500 px-3 py-1 rounded cursor-pointer' to='/signup'>Sign up</NavLink>
+                   
+
+                    <div  className='flex gap-7'>
+                   <div className='flex items-center'>
+                   <NavLink className='relative flex items-center justify-center ' to='/favorites'>
+                            <svg className='w-4 h-4 text-white' fill='none' stroke='currentColor' viewBox='0 0 24 24' xmlns='http://www.w3.org/2000/svg'>
+                                <path strokeLinecap='round' strokeLinejoin='round' strokeWidth='2' d='M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z'></path>
+                            </svg>
+                      </NavLink>
+                   </div>
+                      <div className='hidden sm:flex space-x-3 items-center font-semibold '>
+                      <NavLink className='text-white' to='/login'>Login</NavLink> {/* Use NavLink instead of button */}
+                      <NavLink className='bg-white text-green-500 px-3 py-1 rounded cursor-pointer' to='/signup'>Sign up</NavLink>
+                      </div>
+                       
                     </div>
 
                     {/* Hamburger Menu for Mobile */}
