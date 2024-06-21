4d1c178abdb6dfe38966e7d2aefc6cec56ad18c7
Maha <maha.waqar.warraich@gmail.com>
2024-06-21 10:42:42 +0500

    minor changes and clean up====

--- src/App.js
+++ src/App.js
@@ -5,7 +5,9 @@ import { Routes, Route, useLocation } from "react-router-dom";
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import RecipeDetail from './components/RecipeDetail';
-
+import RecipeForm from './components/RecipeForm';
+import Login from './components/Login';
+import SignUp from './components/SignUp';
 
 function App() {
   const location = useLocation();
@@ -19,7 +21,7 @@ function App() {
         <Route path="/recipe-detail/:id" element={<RecipeDetail />} />
         <Route path="/add-recipe" element={<RecipeForm />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<SignUp />} />
+        <Route path="/SignUp" element={<SignUp />} />
       </Routes>
 
       {location.pathname !== '/' && <Footer />}
